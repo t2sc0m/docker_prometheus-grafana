28552f55aabed2ffdfe4e9855d2de8aa873930dd
kang.minkoo <kang.minkoo@colon.ne.jp>
2016-07-04 18:40:08 +0900

    update for test

--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@
 
 /etc/init.d/grafana-server start
 /make_config.sh 
-/usr/local/prometheus/prometheus -config.file=/usr/local/prometheus/db1.yml
+#/usr/local/prometheus/prometheus -config.file=/usr/local/prometheus/db1.yml
+/usr/local/prometheus/prometheus 
