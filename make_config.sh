fdddf468d66dd156c0af590772d1c80ea3af5c3a
kang.minkoo <kang.minkoo@colon.ne.jp>
2016-07-05 10:32:50 +0900

    monitoring mysql only

--- make_config.sh
+++ make_config.sh
@@ -9,11 +9,11 @@ global:
   scrape_interval:     5s
   evaluation_interval: 5s
 scrape_configs:
-  - job_name: linux
-    target_groups:
-      - targets: ['${addr}:9100']
-        labels:
-          alias: DB${name}
+#  - job_name: linux
+#    target_groups:
+#      - targets: ['${addr}:9100']
+#        labels:
+#          alias: DB${name}
   - job_name: mysql
     target_groups:
       - targets: ['${addr}:9104']
